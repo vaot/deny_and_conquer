b04ecf1b87411e12d745856a8532649216e92682
hugo2121 <wla148@sfu.ca>
2019-03-30 09:47:32 -0700

    change draw style

--- public/grid.js
+++ public/grid.js
@@ -1,65 +1,39 @@
 const { ipcRenderer } = require('electron');
-let started = false, ctx, gridArr_x = [], gridArr_y = [], range = [], last_x, last_y, area = 0, occupyArr = [], start_x, start_y;
+let started = false, ctx, gridArr = [], gridArr_x = [], gridArr_y = [], range = [], last_x, last_y, area = 0, occupyArr = [], start_x, start_y, canvas;
 
-const eventController = {
-    onMouseDown(event) {
-        console.log("mouse down");
-        ctx.beginPath();
 
-        ctx.moveTo(event.layerX, event.layerY);
-        console.log(event.layerX, event.layerY);
-        range = pen.getMouseRange(event.layerX, event.layerY);
-        // console.log("range is", range);
-        started = true;
-        pen.create2DArray();
-        start_x = range[0];
-        start_y = range[2];
-        // console.log("arr is ", occupyArr);
-    },
-    onMouseUp(event) {
-        console.log("mouse up");
-        started = false;
-    },
-    onMouseMove(event) {
-        console.log("mouse move ", event.layerX, event.layerY);
-        ctx.strokeStyle = pen.color;
-        ctx.lineWidth = pen.penWidth;
-        if (started) {
-            if (event.layerX >= range[0] && event.layerX <= range[1] && event.layerY >= range[2] && event.layerY <= range[3]) {
-                ctx.lineTo(event.layerX, event.layerY);
-                ctx.stroke();
-
-                ipcRenderer.send('move', JSON.stringify({
-                    x: event.layerX,
-                    y: event.layerY
-                }));
-
-                last_x = event.layerX;
-                last_y = event.layerY;
-
-                if (pen.occupied()) {
-                    ctx.rect(range[0], range[2], range[1] - range[0], range[3]-range[2]);
-                    ctx.fillStyle = pen.color;
-                    ctx.fill();
-                    started = false;
-
-                    ipcRenderer.send('occupied', {
-                        range: range
-                    });
-                }
-            }
-        }
+module.exports = class Grid {
+    constructor (size, percentage, penColor, penWidth) {
+        this.size = size;
+        this.percentage = percentage;
+        this.penColor = penColor;
+        this.penWidth = penWidth;
+        canvas = document.getElementById("grid");
+        canvas.width = this.size * 50;
+        canvas.height = this.size * 50;
+        if (canvas.getContext) {
+            ctx = canvas.getContext('2d');
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+            ctx.fillRect(0, 0, canvas.width,canvas.height);
+            // console.log(gridArr_x);
+            // console.log(gridArr_y);
+        } else {
+            console.log("not support");
+        }        
+        canvas.onmousedown = event => this.onMouseDown(event);
+        canvas.onmouseup = event => this.onMouseUp(event);
+        canvas.onmousemove = event => this.onMouseMove(event);
     }
-};
 
-const pen = {
-    color: 'rgb(255, 165, 0)',
-    penWidth: 5,
-    dx: 50,
-    dy: 50,
-    drawGrid(w, h) {
-        var dx = this.dx;
-        var dy = this.dy;
+    getColor() { return this.penColor}
+
+    getWidth() {return this.penWidth}
+
+    drawGrid() {
+        var w = canvas.width;
+        var h = canvas.height;
+        var dx = 50;
+        var dy = 50;
 
         var x = 0;
         var y = 0;
@@ -89,7 +63,67 @@ const pen = {
         }
         gridArr_x.unshift(0);
         gridArr_y.unshift(0);
-    },
+        gridArr = this.create2DArray(gridArr_x.length, gridArr_y.length);
+    }
+
+    onMouseDown(event) {
+        console.log("mouse down");
+        ctx.beginPath();
+
+        ctx.moveTo(event.layerX, event.layerY);
+        console.log(event.layerX, event.layerY);
+        range = this.getMouseRange(event.layerX, event.layerY);
+        // console.log("range is", range);
+        started = true;
+        occupyArr = this.create2DArray(Math.floor(50 / this.penWidth), Math.floor(50 / this.penWidth));
+        
+        // console.log(gridArr);
+        start_x = range[0];
+        start_y = range[2];        
+    }
+
+    onMouseUp(event) {
+        console.log("mouse up");
+        started = false;
+    }
+
+    onMouseMove(event) {
+        // console.log("mouse move ", event.layerX, event.layerY);
+        ctx.strokeStyle = this.penColor;
+        ctx.lineWidth = this.penWidth;
+        console.log(ctx.lineWidth);
+        if (started && gridArr[range[0]/50][range[2]/50] == 0) {
+            if (event.layerX >= range[0] + 0.3 && event.layerX <= range[1] -0.3 && event.layerY >= range[2] + 0.3 && event.layerY <= range[3] - 0.3) {
+                ctx.lineTo(event.layerX, event.layerY);
+                ctx.stroke();
+
+                ipcRenderer.send('move', JSON.stringify({
+                    x: event.layerX,
+                    y: event.layerY
+                }));
+
+                last_x = event.layerX;
+                last_y = event.layerY;
+
+                // console.log("sdfsdfsdfsdfsdf",occupyArr);
+                if (this.occupied()) {
+                    ctx.rect(range[0], range[2], range[1] - range[0], range[3]-range[2]);
+                    ctx.fillStyle = this.penColor;
+                    ctx.fill();
+                    started = false;
+                    var xIndex = range[0] / 50;
+                    var yIndex = range[2] / 50;
+                    gridArr[xIndex][yIndex] = 1;
+                    // console.log(range);
+                    // console.log("x, y", xIndex, yIndex);
+                    ipcRenderer.send('occupied', {
+                        range: range
+                    });
+                }
+            }
+        }
+    }
+
     getMouseRange(x, y) {
         var min_x = 0, max_x = 0, min_y = 0, max_y = 0, rangeArr = [];
         for (var i=0; i<gridArr_x.length - 1;i++) {
@@ -105,32 +139,23 @@ const pen = {
             }
         }
         rangeArr.push(min_x, max_x,min_y, max_y);
-        return rangeArr;
-    },
-    create2DArray() {
-        var row = Math.floor(this.dx / this.penWidth);
-        var col = Math.floor(this.dy / this.penWidth);
-        occupyArr = new Array(row);
+        return rangeArr;        
+    }
+
+    create2DArray(row, col) {
+        var arr2D = new Array(row);
         for (var i=0; i<row; i++) {
-            occupyArr[i] = new Array(col);
+            arr2D[i] = new Array(col);
             for (var j=0; j<col; j++) {
-                occupyArr[i][j] = 0;
+                arr2D[i][j] = 0;
             }
         }
-    },
-    fill2DArray() {
-        var xPos = last_x - start_x;
-        var yPos = last_y - start_y;
-        var xIndex = Math.floor(xPos / this.penWidth);
-        var yIndex = Math.floor(yPos / this.penWidth);
-        var row = occupyArr.length;
-        var col = occupyArr[0].length;
-        if (xIndex < row && yIndex < col )
-            occupyArr[xIndex][yIndex] = 1;
-    },
+
+        return arr2D;
+    }
     occupied() {
         this.fill2DArray();
-        var percentage = 0.5;
+        var percentage = this.percentage;
         var row = occupyArr.length;
         var col = occupyArr[0].length;
         var threshold = row * col * percentage;
@@ -145,29 +170,16 @@ const pen = {
         }
         return false;
     }
-};
 
-
-function initCanvas() {
-    const canvas = document.getElementById("grid");
-
-    if (canvas.getContext) {
-        ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
-        ctx.fillRect(0, 0, canvas.width,canvas.height);
-        var w = canvas.width;
-        var h = canvas.height;
-        pen.drawGrid(w, h);
-        canvas.onmousedown = event => eventController.onMouseDown(event);
-        canvas.onmouseup = event => eventController.onMouseUp(event);
-        canvas.onmousemove = event => eventController.onMouseMove(event);
-        // console.log(gridArr_x);
-        // console.log(gridArr_y);
-    } else {
-        console.log("not support");
+    fill2DArray() {
+        var xPos = last_x - start_x;
+        var yPos = last_y - start_y;
+        var xIndex = Math.floor(xPos / this.penWidth);
+        var yIndex = Math.floor(yPos / this.penWidth);
+        var row = occupyArr.length;
+        var col = occupyArr[0].length;
+        if (xIndex < row && yIndex < col )
+            occupyArr[xIndex][yIndex] = 1;
     }
+
 }
-
-
-window.onload = initCanvas();
-module.exports = { controller: eventController, pen: pen };
